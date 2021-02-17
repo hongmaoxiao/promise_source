c62657d40aa14cb269cb655c08db5aa0760b96a6
hongmaoxiao <769501294@qq.com>
2021-02-17 21:46:46 +0800

    Add extensions and documentation

--- index.js
+++ index.js
@@ -1,119 +1,84 @@
-var nextTick
+//This file contains then/promise specific extensions to the core promise API
 
-if (typeof setImmediate === 'function') { // IE >= 10 & node.js >= 0.10
-  nextTick = function(fn){ setImmediate(fn) }
-} else if (typeof process !== 'undefined' && process && typeof process.nextTick === 'function') { // node.js before 0.10
-  nextTick = function(fn){ process.nextTick(fn) }
-} else {
-  nextTick = function(fn){ setTimeout(fn, 0) }
-}
+var Promise = require('./core.js')
+var nextTick = require('./lib/next-tick')
 
 module.exports = Promise
-function Promise(fn) {
-  if (!(this instanceof Promise)) return new Promise(fn)
-  if (typeof fn !== 'function') {
-    throw new TypeError('fn is not a function')
+
+/* Static Functions */
+
+Promise.from = function (value) {
+  if (value instanceof Promise) {
+    return value
   }
 
-  var state = null
-  var delegating = false
-  var value = null
-  var deferreds = []
-  var self = this
+  return new Promise(function(resolve) {
+    resolve(value)
+  })
+}
 
+Promise.denodeify = function(fn) {
+  return function () {
+    var self = this
+    var args = Array.prototype.slice(arguments)
 
-  this.then = function(onFulfilled, onRejected) {
     return new Promise(function(resolve, reject) {
-      handle(new Handler(onFulfilled, onRejected, resolve, reject))
-    })
-  }
-
-  function handle(deferred) {
-    if (state === null) {
-      deferreds.push(deferred)
-      return
-    }
-    nextTick(function() {
-      var cb = state ? deferred.onFulfilled : deferred.onRejected
-      if (cb === null) {
-        (state ? deferred.resolve : deferred.reject)(value)
-        return
-      }
-      var ret
-      try {
-        ret = cb(value)
-      } catch (e) {
-        deferred.reject(e)
-        return
-      }
-      deferred.resolve(ret)
-    })
-  }
-
-  function resolve(newValue) {
-    if (delegating) {
-      return
-    }
-    resolve_(newValue)
-  }
-
-  function resolve_(newValue) {
-    if (state !== null) {
-      return
-    }
-    try { //Promise Resolution Procedure: https://github.com/promises-aplus/promises-spec#the-promise-resolution-procedure
-      if (newValue === self) {
-        throw new TypeError('A promise cannot be resolved with itself.')
-      }
-      if (newValue && (typeof newValue === 'object' || typeof newValue === 'function')) {
-        var then = newValue.then
-        if (typeof then === 'function') {
-          delegating = true
-          then.call(newValue, resolve_, reject_)
-          return
+      args.push(function(err, res) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(res)
         }
-      }
-      state = true
-      value = newValue
-      finale()
-    } catch (e) {
-      reject_(e)
-    }
-  }
-
-  function reject(newValue) {
-    if (delegating) {
-      return
-    }
-    reject_(newValue)
-  }
-
-  function reject_(newValue) {
-    if (state !== null) {
-      return
-    }
-    state = false
-    value = newValue
-    finale()
-  }
-
-  function finale() {
-    for (let i = 0; i < deferreds.length; i++) {
-      handle(deferreds[i])
-    }
-    deferreds = null
-  }
-
-  try {
-    fn(resolve, reject)
-  } catch (e) {
-    reject(e)
+      })
+      fn.apply(self, args)
+    })
   }
 }
 
-function Handler(onFulfilled, onRejected, resolve, reject) {
-  this.onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : null
-  this.onRejected = typeof onRejected === 'function' ? onRejected : null
-  this.resolve = resolve
-  this.reject = reject
+Promise.nodeify = function(fn) {
+  return function () {
+    var args = Array.prototype.slice(arguments)
+    var callback = typeof args[args.length - 1] === 'function' ? args.pop() : null
+
+    try {
+      return fn.apply(this, arguments).nodeify(callback)
+    } catch (ex) {
+      if (callback == null) {
+        return new Promise(function(resolve, reject) {
+          reject(ex)
+        })
+      } else {
+        nextTick(function() {
+          callback(ex)
+        })
+      }
+    }
+  }
+}
+
+/* Prototype Methods */
+
+Promise.prototype.done = function(onFulfilled, onRejected) {
+  var self = arguments.length ? this.then.apply(this, arguments) : this
+  self.then(null, function(err) {
+    nextTick(function() {
+      throw err
+    })
+  })
+}
+
+Promise.prototype.nodeify = function (callback) {
+  if (callback == null) {
+    return this
+  }
+
+  this.then(function(value) {
+    nextTick(function() {
+      callback(null, value)
+    })
+  }, function(err) {
+    nextTick(function() {
+      callback(err)
+    })
+  })
 }
