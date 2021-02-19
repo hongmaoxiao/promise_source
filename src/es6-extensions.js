3c2d1f51b0f313fe190614731486de2fe73bda88
hongmaoxiao <769501294@qq.com>
2021-02-19 13:18:52 +0800

    Add builds for domains and setImmedaite

--- src/es6-extensions.js
+++ src/es6-extensions.js
@@ -3,7 +3,7 @@
 //This file contains then/promise specific extensions to the core promise API
 
 var Promise = require('./core.js')
-var asap = require('asap')
+var asap = require('asap/raw')
 
 
 module.exports = Promise
---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = require('./lib/core.js')
+require('./lib/done.js')
+require('./lib/finally.js')
+require('./lib/es6-extensions.js')
+require('./lib/node-extensions.js')
